f51b3711ec09742c0fccf7597fcaed965f1afb2e
suryanagendra <suryanagendra446@gmail.com>
2018-04-21 10:28:11 +0530

    Search Method

---
+++ Strings/search/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var str = "surya nagendra";
+var inp = "nagendra";
+console.log("Source text "+ str)
+console.log("Input text is "+ inp)
+var out = str.search(inp);
+console.log("Output is "+ out);
